f14b9ee960222c66d6f743f08d45a9530074168a
huynguyen <huy9701@gmail.com>
2019-03-22 12:07:43 +0700

    test deploy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Huy Dep Trai
           </a>
         </header>
       </div>
