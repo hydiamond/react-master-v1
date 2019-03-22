25ec5d59f126ba8e39d06330ec7ff42ad7e9470f
huynguyen <huy9701@gmail.com>
2019-03-22 18:25:04 +0700

    eb-deploy !

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Huy Dep Trai
+            Xin chao ban Huy
           </a>
         </header>
       </div>
